e9b952fef287202609fafcf8ffc5335cc78fafd3
yugshah7777 <yugshah7777@gmail.com>
2026-05-28 00:34:29 +0530

    Fix Google Auth Route, vercel spa routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import CitizenDashboard from "./components/ui/CitizenDashboard";
 import StaffDashboard from "./components/ui/StaffDashboard";
 import AdminDashboard from "./components/ui/AdminDashboard";
 import { ToastProvider } from "./components/ui/Toast";
+import AuthSuccess from "./components/ui/AuthSuccess";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/citizen-dashboard" element={<CitizenDashboard />} />
           <Route path="/staff-dashboard" element={<StaffDashboard />} />
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/auth-success" element={<AuthSuccess />} />
         </Routes>
       </div>
     </ToastProvider>
