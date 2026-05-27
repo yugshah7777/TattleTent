46b2f6d79936305e7744ab07ffa1a97cebf5c3e1
yugshah7777 <yugshah7777@gmail.com>
2026-05-27 23:17:26 +0530

    DB connected to NeonDB

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,19 +4,19 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // load variables from .env
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
-
 // const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
 // });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
