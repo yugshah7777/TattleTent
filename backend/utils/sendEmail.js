0865b866c6b4a4795dba885c72ccdd613fa22a33
yugshah7777 <yugshah7777@gmail.com>
2026-05-28 06:45:19 +0530

    adjust smtp config

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,28 +1,27 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  host: "smtp.gmail.com",
   port: 587,
   secure: false,
-  requireTLS: true,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  tls: {
-    rejectUnauthorized: false,
-  },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 30000,
 });
 
-transporter.verify((err, success) => {
-  if (err) {
-    console.error('SMTP VERIFY ERROR:', err);
-  } else {
-    console.log('SMTP READY');
-  }
-});
+// transporter.verify((err, success) => {
+//   if (err) {
+//     console.error('SMTP VERIFY ERROR:', err);
+//   } else {
+//     console.log('SMTP READY');
+//   }
+// });
 
 const sendEmail = async (options) => {
   try {
